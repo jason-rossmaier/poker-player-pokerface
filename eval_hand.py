d18ab769967f63da1b30aa313116c7913af96be4
Daniel DePaolo <ddepaolo@newfieldwireless.com>
2016-07-21 11:24:12 -1000

    more eval_hand changes.

--- eval_hand.py
+++ eval_hand.py
@@ -4,32 +4,37 @@ from collections import defaultdict
 #for straights and flushes, (highest card / 13) is added
 ###########
 hand_str_dict = {
-    'high card': 0,
-    'pair' : 1,
-    'two_pair' : 2,
-    'three_kind' : 3,
-    'straight' : 4,
-    'flush' : 5,
-    'full_house' : 6,
-    'four_kind' : 7,
-    'straight_flush' : 8,
+    'high card': 0.0,
+    'pair' : 1.0,
+    'two_pair' : 2.0,
+    'three_kind' : 3.0,
+    'straight' : 4.0,
+    'flush' : 5.0,
+    'full_house' : 6.0,
+    'four_kind' : 7.0,
+    'straight_flush' : 8.0,
 }
 
-# range 0-1, float. 1 is flush, 0.4 is 2 cards of same suit
+# range 0-1, float. flush defined above, 0.4 is 2 cards of same suit, 0.6 3 cards of same suit
 def eval_flush(cards):
     card_suits = defaultdict(int)
     for card in cards:
         suit = card["suit"]
         card_suits[suit] += 1
-        
+
     num_flush_cards = max(card_suits.items(), key = lambda x: x[1])[1]
-    if (num_flush_cards) == 1: #just having 1 card of a particular suit is baseline and makes no difference
+    #just having 1 card of a particular suit is baseline and makes no difference
+    if (num_flush_cards) == 1: 
         num_flush_cards = 0
+    #we have a flush! automatically big points
+    if num_flush_cards >= 5:
+        return hand_str_dict['flush']
+
     return float(num_flush_cards) / 5.0
 
 def eval_hand_priv(cards):
     result = 0.0
-    
+
     card_ranks = defaultdict(int)
     for card in cards:
         rank = card["rank"]
@@ -49,8 +54,8 @@ def eval_hand(hole, community):
 
 if __name__ == "__main__":
     community = [{"rank":2, "suit":'C'}]
-    hole = [{"rank":11, "suit":'C'},{"rank":2, "suit":'D'}]
+    hole = [{"rank":11, "suit":'C'},{"rank":2, "suit":'C'},{"rank":11, "suit":'C'},{"rank":2, "suit":'C'}]
     result = eval_hand(hole, community)
-    
+
     print "hand result:",result
     print "flush cards: ", eval_flush(hole + community)
