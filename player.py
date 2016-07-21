ca02cd53fac51e092244069721eee2d5b72cf9b8
Di-An Jan <djan@netscout.com>
2016-07-21 11:15:57 -0700

    use dict access for JSON

--- player.py
+++ player.py
@@ -1,3 +1,5 @@
+import sys
+sys.stdout = sys.stderr
 
 class Player:
     VERSION = "Default Python folding player"
@@ -12,12 +14,12 @@ class Player:
         min_bet = current_buy_in - our_player["bet"]
 
         #pre-flop
-        if game_state.round == 0:
-            cards = our_player.hole_cards
+        if game_state["round"] == 0:
+            cards = our_player["hole_cards"]
             #if we have at least one ace or a pair, we bet the first hand, if not, then fold
-            if (cards[0].rank == 'A'
-                or cards[1] == 'A'
-                or cards[0].rank == cards[1].rank):
+            if (cards[0]["rank"] == 'A'
+                    or cards[1] == 'A'
+                    or cards[0]["rank"] == cards[1]["rank"]):
                 return min_bet + minimum_raise
             else:
                 return 0
