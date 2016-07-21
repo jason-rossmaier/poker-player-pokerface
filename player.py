078ff64a912771c8a0723c1f3c461448fbede6fa
Di-An Jan <djan@newwireless.com>
2016-07-21 10:39:07 -0700

    always bet

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 0
+        return 10
 
     def showdown(self, game_state):
         pass
